d0fa2cd973166374846843847ff725326ac19957
ankit98710@ <ankit2409.ak@gmail.com@gmail.com>
2020-12-09 01:18:10 +0530

    green for number greater than 1000

--- TestTDDCalculator/src/com/TDD/calculator/Calculator.java
+++ TestTDDCalculator/src/com/TDD/calculator/Calculator.java
@@ -9,16 +9,18 @@ public class Calculator {
 	   }  
 	public int add(String s) {
 		String ar[]=null;
-		System.out.println(s.charAt(4));
+		//System.out.println(s.charAt(4));
 		if (s.charAt(0)=='/' && s.charAt(1)=='/' &&s.charAt(3)=='\n') {
 			String n = s.substring(4);
 			ar = n.split(Character.toString(s.charAt(2)));
 		}else {
 			ar = s.split("[\\n,]+");}
-		System.out.println(Arrays.toString(ar));
+		//System.out.println(Arrays.toString(ar));
 		int sum=0;
 		for(int i=0;i<ar.length;i++) {
 			int num=Integer.parseInt(ar[i]);
+			if(num>1000)
+				continue;
 			try {
 				validate(num);
 				sum+=num;

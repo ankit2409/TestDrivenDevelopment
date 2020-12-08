2dfb417cfbbd71eeec9ff291f59306029fbecd46
ankit98710@ <ankit2409.ak@gmail.com@gmail.com>
2020-12-09 00:53:11 +0530

    Green for default delimeter

--- TestTDDCalculator/src/com/TDD/calculator/AddTest.java
+++ TestTDDCalculator/src/com/TDD/calculator/AddTest.java
@@ -9,8 +9,8 @@ class AddTest {
 	@Test
 	void test() {
 		Calculator calculator = new Calculator();
-		int result = calculator.add("1\n2,3,4\n5");
-		assertEquals(15,result);
+		int result = calculator.add("1,1\n2");
+		assertEquals(4,result);
 	}
 
 }
--- TestTDDCalculator/src/com/TDD/calculator/Calculator.java
+++ TestTDDCalculator/src/com/TDD/calculator/Calculator.java
@@ -1,8 +1,17 @@
 package com.TDD.calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
 	public int add(String s) {
-		String ar[]=s.split("[\\n,]+");
+		String ar[]=null;
+		System.out.println(s.charAt(4));
+		if (s.charAt(0)=='/' && s.charAt(1)=='/' &&s.charAt(3)=='\n') {
+			String n = s.substring(4);
+			ar = n.split(Character.toString(s.charAt(2)));
+		}else {
+			ar = s.split("[\\n,]+");}
+		System.out.println(Arrays.toString(ar));
 		int sum=0;
 		for(int i=0;i<ar.length;i++) {
 			sum+=Integer.parseInt(ar[i]);

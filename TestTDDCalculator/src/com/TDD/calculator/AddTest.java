907d013f6848e5dd04e1a9131ab530486d38eed2
ankit98710@ <ankit2409.ak@gmail.com@gmail.com>
2020-12-09 00:15:28 +0530

    Red while using \n as delimeter

--- TestTDDCalculator/src/com/TDD/calculator/AddTest.java
+++ TestTDDCalculator/src/com/TDD/calculator/AddTest.java
@@ -9,8 +9,8 @@ class AddTest {
 	@Test
 	void test() {
 		Calculator calculator = new Calculator();
-		int result = calculator.add("8,9,1,2,4,2,3");
-		assertEquals(29,result);
+		int result = calculator.add("8,1\n2");
+		assertEquals(11,result);
 	}
 
 }
